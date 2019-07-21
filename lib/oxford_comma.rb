fa7c8deaa3216d971d71694f82047f22a873d81f
Patrick Harrison <jahman@bellatlantic.net>
2019-07-21 21:35:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+ # ["kiwi"].join
+# def oxford_comma(array,bill)
+# ["kiwi"," and durian"].join
+# end
+# oxford_comma("array","and durian")
+# def oxford_comma(array)
+["kiwi"].join << " and durian"
 
-end
+ end
+# def oxford_comma(array)
+#   ["kiwi"].join << ", durian,and starfruit"
+
+# end
